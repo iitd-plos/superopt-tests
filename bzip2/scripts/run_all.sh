a3dea28f424f130cb797c58c125037019e8ae4dd
Sorav Bansal <sbansal@iitd.ac.in>
2019-12-13 14:09:03 +0530

    fixed path in bzip2/scripts/run_all.sh

--- bzip2/scripts/run_all.sh
+++ bzip2/scripts/run_all.sh
@@ -4,7 +4,7 @@ CORES=$(nproc)
 CORES=$((CORES/3))
 
 # XXX change this!
-export SUPEROPT_PROJECT_ROOT=${HOME}/thesis/superopt-project
+export SUPEROPT_PROJECT_ROOT=${SUPEROPT_ROOT}
 
 ./parallel-bzip2.sh -c ${CORES} -O'-c clang -P"--unroll-factor 4"'  eq_funcs_spec2k_bzip2_ext_v2
 ./parallel-bzip2.sh -c ${CORES} -O'-c clang -P"--unroll-factor 16"' eq_funcs_spec2k_bzip2_ext_v8
