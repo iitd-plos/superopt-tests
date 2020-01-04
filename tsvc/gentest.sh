576c5a79be9804528ca347cb473964f1b32ec624
Sorav Bansal <sbansal@iitd.ac.in>
2020-01-05 00:28:36 +0530

    tsvc/gentest.sh : do not pass noclobber flag
    noclobber prevents the harvest file from being regenerated which is not desirable

--- tsvc/gentest.sh
+++ tsvc/gentest.sh
@@ -4,6 +4,6 @@ set -eu # fail on error
 
 # generate .etfg and .tfg files 
 python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n  tsvc.bc.O0.s tsvc.clang.eqchecker.O3.i386
-# passing --noclobber so that .etfg is not regenerated
-python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n --noclobber tsvc.bc.O0.s tsvc.gcc.eqchecker.O3.i386
+# TODO: pass a flag so that .etfg is not regenerated
+python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n tsvc.bc.O0.s tsvc.gcc.eqchecker.O3.i386
 python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n  tsvc_icc.bc.O0.s tsvc_icc.icc.eqchecker.O3.i386
