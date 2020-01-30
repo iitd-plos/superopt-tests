0738829c33efa41c76d3521e81f6d2ac2386fd27
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2020-01-30 21:41:18 +0530

    micro: added supported for compiler specific eqflags

--- micro/gentest.sh
+++ micro/gentest.sh
@@ -11,7 +11,20 @@ gen_for_src_dst()
   python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n ${infile_pfx}_src.bc.O0.s ${infile_pfx}_dst.icc.eqchecker.O3.i386
 }
 
+gen_for_all()
+{
+  file_pfx="$1"
+  python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n ${file_pfx}.bc.O0.s ${file_pfx}.gcc.eqchecker.O3.i386
+  python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n ${file_pfx}.bc.O0.s ${file_pfx}.clang.eqchecker.O3.i386
+  python ${SUPEROPT_PROJECT_DIR}/superopt/utils/eqbin.py -n ${file_pfx}.bc.O0.s ${file_pfx}.icc.eqchecker.O3.i386
+}
+
 for f in ${PROGS_PREFIX}
 do
   gen_for_src_dst ${f}
+done
+
+for f in ${PROGS}
+do
+  gen_for_all ${f}
 done
