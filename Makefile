f15c92f49cc86a5cda848a3616145ccb7b3e3eb3
Sorav Bansal <sbansal@iitd.ac.in>
2020-03-04 18:41:42 +0530

    added codegen_test Makefile target
    The codegen_test target is used in the compcert-tests directory
    that contains some of the compcert test files for which the
    codegen is supposed to succeed

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 include config-host.mak      # BUILDDIR
 
 # add new dirs' targets here
-TARGETS := bzip2 tsvc semalign reve micro
+EQCHECK_TARGETS := bzip2 tsvc semalign reve micro
+CODEGEN_TARGETS := compcert-tests
+TARGETS := $(EQCHECK_TARGETS) $(CODEGEN_TARGETS)
 
 # rules
 
-all: $(BUILDDIR) $(TARGETS)
+all: $(BUILDDIR) $(TARGETS) $(CODEGEN_TARGETS)
 
 clean:
 	$(foreach t,$(TARGETS),make -C $(BUILDDIR)/$(t) clean;)
@@ -16,7 +18,7 @@ distclean: clean
 $(BUILDDIR):
 	mkdir -p $@
 
-$(TARGETS):
+$(TARGETS) $(CODEGEN_TARGETS):
 	mkdir -p $(BUILDDIR)/$@
 	cp $@/Makefile -t $(BUILDDIR)/$@
 	make -C $(BUILDDIR)/$@
@@ -26,5 +28,11 @@ gentest:
 
 runtest:
 	$(foreach t,$(TARGETS),make -C $(BUILDDIR)/$(t) runtest || exit;)
+
+typecheck_test:
+	$(SUPEROPT_PROJECT_DIR)/superopt/typecheck
+
+codegen_test:
+	$(foreach t,$(CODEGEN_TARGETS),make -C $(BUILDDIR)/$(t) codegen_test || exit;)
 
 .PHONY: all clean distclean $(TARGETS) gentest runtest
---
+++ scripts/codegen_test.py
@@ -0,0 +1,84 @@
+import multiprocessing
+import sys
+import os
+import time
+import argparse
+from subprocess import check_call, STDOUT
+
+parser = argparse.ArgumentParser(description='Generate codegen test commands.')
+
+parser.add_argument('--superopt_root', type=str, default='/home/sbansal/superopt-project',
+                   help='Superopt root directory')
+parser.add_argument('--prefix', type=str, default='',
+                   help='Prefix directory for the files to be read/written')
+parser.add_argument('progs', nargs='*', default=[], help='List of program names')
+
+args = parser.parse_args()
+
+#execfile("../smpbench-build/config.py")
+#superopt_root = os.path.abspath(srcdir + "/../superopt-project")
+superopt_root = os.path.abspath(args.superopt_root)
+superopt_dir = os.path.abspath(superopt_root + "/superopt")
+superopt_tests_dir = os.path.abspath(superopt_root + "/superopt-tests")
+superopt_tests_build = os.path.abspath(superopt_tests_dir + "/build")
+prefix = args.prefix
+lli = "lli-3.4"
+llvm2tfg_build_dir = os.path.abspath(superopt_root + "/llvm-build")
+
+color_red="`tput bold; tput setaf 1`"
+color_green="`tput bold; tput setaf 2`"
+color_reset="`tput sgr0`"
+#opts = ["O0", "O3"]
+#compilers = ["gcc", "clang", "llc", "icc"]
+opts = ["O3"]
+compilers = ["llc"]
+
+#num_cpus = multiprocessing.cpu_count()
+cint_progs = []
+cint_profile_commands = {}
+
+#for arg in sys.argv:
+for arg in args.progs:
+  cint_progs.append(arg)
+#cint_progs.pop(0)
+
+#print("num_cpus = ", num_cpus)
+for cp in cint_progs:
+  name = os.path.basename(cp)
+  cint_prog = os.path.abspath(superopt_tests_build + "/" + prefix + "/" + cp)
+  #print(cint_prog)
+  cmd = "/bin/true && (/bin/true"
+  for opt in opts:
+    cmd = cmd + " && ("
+    cmd = cmd + llvm2tfg_build_dir + "/bin/llvm2tfg " + cint_prog + ".bc." + opt + ".cg -o " + cint_prog + ".bc." + opt + ".cg.etfg";
+    cmd = cmd + " && " + "rm -f " + cint_prog + ".bc." + opt + ".cg.i386";
+    cmd = cmd + " && " + superopt_dir + "/build/etfg_i386/codegen " + cint_prog + ".bc." + opt + ".cg.etfg -o " + cint_prog + ".bc." + opt + ".cg.i386" + " -l " + cint_prog + ".bc." + opt + ".cg.rewrite.log";
+    cmd = cmd + ")"
+  cmd = cmd + ")"
+  cmd = cmd + " && " + "(perl " + superopt_tests_dir + "/scripts/compare_commands.pl " + name
+  ccmd = ""
+  cint_profile_commands[cp] = []
+  for c in compilers:
+    for opt in opts:
+      ccmdo = "\"" + cint_prog + "." + c + "." + opt + ".i386\""
+      ccmd = ccmd + " " + ccmdo
+      cint_profile_commands[cp].append((c,opt,ccmdo))
+  for opt in opts:
+    ccmdo = "\"" + cint_prog + ".bc." + opt + ".cg.i386\""
+    ccmd = ccmd + " " + ccmdo
+    cint_profile_commands[cp].append(("scg",opt,ccmdo))
+  ccmd = ccmd + " \"\""
+  cmd = cmd + ccmd + " && echo \"" + cint_prog  + " " + color_green + "passed" + color_reset + "\") || echo \"" + cint_prog + " " + color_red + "FAILED" + color_reset + "\""
+  print cmd + "\n"
+
+#DEVNULL = open(os.devnull, 'wb', 0)
+profile_cmds = open("profile_cmds", 'w+')
+for cp,commands in cint_profile_commands.items():
+  profile_cmds.write(cp + "\n")
+  for c,opt,command in commands:
+    profile_cmds.write("  " + c + " : " + opt + " : " + command + "\n")
+  #start = time.time()
+  #check_call([command], stdout=DEVNULL, stderr=STDOUT)
+  #print(command + ": ")
+  #print("{:.3f} seconds".format(time.time() - start))
+profile_cmds.close()
