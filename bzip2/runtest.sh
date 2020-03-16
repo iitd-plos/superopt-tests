f208c4fa4c47ce5abfd02d66057ed9b79ed11c69
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2020-03-16 11:53:10 +0530

    bzip2: added back parallel command mistakenly deleted in last commit

--- bzip2/runtest.sh
+++ bzip2/runtest.sh
@@ -18,3 +18,5 @@ compiler=clang
 gen_commands eq_funcs_spec2k_bzip2_ext_nv ""                   >> chaperon_commands
 gen_commands eq_funcs_spec2k_bzip2_ext_v2 "--unroll-factor 4"  >> chaperon_commands
 gen_commands eq_funcs_spec2k_bzip2_ext_v8 "--unroll-factor 16" >> chaperon_commands
+
+parallel --load "${PARALLEL_LOAD_PERCENT:-100}%" < chaperon_commands
