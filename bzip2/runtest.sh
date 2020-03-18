73cc7f1cac4c0d118ed3ab8bdf07c8172065d0e6
Sorav Bansal <sbansal@iitd.ac.in>
2020-03-18 14:49:45 +0530

    removed unmerged comments in bzip2/runtest.sh

--- bzip2/runtest.sh
+++ bzip2/runtest.sh
@@ -9,15 +9,7 @@ g_eqflags["qSort3"]="--smt-query-timeout 600" # timeouts otherwise
 
 ###########################
 
-<<<<<<< HEAD
-    echo "python ${SUPEROPT_PROJECT_DIR}/superopt/utils/chaperon.py --logfile \"${EQLOGS}/${binary}.${func}.${compiler}.O3.eqlog\" \"${SUPEROPT_PROJECT_DIR}/superopt/build/etfg_i386/eq -f ${func} ${eq_opts} ${binary}.bc.eqchecker.O0.ll.ALL.etfg ${binary}.${compiler}.eqchecker.O3.i386.ALL.tfg\""
-  done < ${infile}
-}
-
-mkdir -p ${EQLOGS}
-=======
 source ${SUPEROPT_PROJECT_DIR}/superopt-tests/scripts/eqchecker_runtest_from_file.sh
->>>>>>> origin/ipa
 
 binary=bzip2
 compiler=clang
