83b795d117a8ed8f20394c6e86a7ac5db633e010
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2020-03-22 01:24:25 +0530

    bzip2_minimal_changes: added eqflags

--- bzip2_minimal_changes/runtest.sh
+++ bzip2_minimal_changes/runtest.sh
@@ -9,6 +9,10 @@ g_eqflags["qSort3"]="--smt-query-timeout 600" # timeouts otherwise
 #g_eqflags["loadAndRLEsource"]="--unroll-factor 16" # higher unroll-factor required on exit path
 #g_eqflags["getAndMoveToFrontDecode"]="--unroll-factor 16" # higher unroll-factor required on exit path
 
+g_global_eqflags=" --global-timeout 7200"  # everything above this should be terminated anyway
+g_global_eqflags="${g_global_eqflags:-} --disable_query_decomposition" # hasn't been useful
+g_global_eqflags="${g_global_eqflags:-} --disable_residual_loop_unroll" # path explosion problem
+
 ###########################
 
 source ${SUPEROPT_PROJECT_DIR}/superopt-tests/scripts/eqchecker_runtest_from_file.sh
