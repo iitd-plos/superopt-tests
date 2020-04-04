558d97513723e1cc5627fdd930e59d79d7de337f
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2020-04-05 02:49:21 +0530

    tsvc/s254: divide by 2 rather than multiplying by .5 which is equal to 0

--- tsvc/tsvc.c
+++ tsvc/tsvc.c
@@ -1201,15 +1201,16 @@ int s253()
 int s254()
 {
 
-//	scalar and array expansion
-//	carry around variable
+  //	scalar and array expansion
+  //	carry around variable
 
 	TYPE x;
-		x = b[LEN-1];
-		for (int i = 0; i < LEN; i++) {
-			a[i] = (b[i] + x) * (TYPE).5;
-			x = b[i];
-		}
+	x = b[LEN-1];
+	for (int i = 0; i < LEN; i++) {
+		//a[i] = (b[i] + x) * (TYPE).5;
+		a[i] = (b[i] + x) / 2;
+		x = b[i];
+	}
 	return 0;
 }
 
