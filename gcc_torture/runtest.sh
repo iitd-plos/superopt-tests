2640aab337a59005801fee96d9b83ad1a845644a
ShubhaniGupta <gupta.shubhani@gmail.com>
2020-05-31 17:01:31 +0530

    Added gcc torture tests. Total 1554 functions -- 27 functions with VLA's or functions with variable arguments, 161 functions which have gcc specific features and cannot be compiled by other compilers. Currently try to run EQ for remaining 1366 functions

---
+++ gcc_torture/runtest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu # fail on error
+
+###########################
+
+# function specific eqflags
+# NOTE: compiler specific version (with .<compiler> suffix) overrides the normal one
+declare -A g_eqflags
+g_global_eqflags="--global-timeout 7200 --smt-query-timeout 1800" # should be enough for most
+
+###########################
+
+source ${SUPEROPT_PROJECT_DIR}/superopt-tests/scripts/eqchecker_runtest.sh
+
+> chaperon_commands
+for f in ${PROGS}
+do
+  gen_for_all ${f} >> chaperon_commands
+done
+
+#parallel --load "${PARALLEL_LOAD_PERCENT:-100}%" < chaperon_commands
