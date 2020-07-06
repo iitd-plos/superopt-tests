05626bd759f68e08e25c75fa1e31ca9921604bc0
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2020-07-06 18:26:05 +0530

    micro/vla: disable use of lsprel mapping file as lsprels can now be inferred from DWARF headers
    ICC does not generate precise DWARF headers so we still need to supply
    lsprel mappings for icc generated binaries

--- micro/vla.c
+++ micro/vla.c
@@ -1,3 +1,6 @@
+#include "eqchecker_helper.h"
+#include <stdarg.h>
+
 int foo_vla(int *a, unsigned n)
 {
   int v[n];
@@ -5,9 +8,50 @@ int foo_vla(int *a, unsigned n)
   for (unsigned i = 0; i < n; ++i) {
     v[i] = a[i]*a[i];
   }
-  return v[0]*v[n-1]*sizeof(v);
+  return /*v[0]**/v[n-1]*sizeof(v);
 }
 
+//int multi_vla(int* a, unsigned n)
+//{
+//  if (a[0] > 0) {
+//    int v[n];
+//#pragma clang loop vectorize(disable)
+//    for (unsigned i = 0; i < n; ++i) {
+//      v[i] = a[i]*a[i];
+//    }
+//    return v[0]*v[n-1]*sizeof(v);
+//  } else {
+//    int v[n+1];
+//    v[0] = -1;
+//#pragma clang loop vectorize(disable)
+//    for (unsigned i = 0; i < n; ++i) {
+//      v[i+1] = a[i]*a[i];
+//    }
+//    return v[0]*v[n]*sizeof(v);
+//  }
+//}
+
+void baz_vararg(const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+ 
+    while (*fmt != '\0') {
+        if (*fmt == 'd') {
+            int i = va_arg(args, int);
+            printf("%d\n", i);
+        } else if (*fmt == 'c') {
+            // A 'char' variable will be promoted to 'int'
+            // A character literal in C is already 'int' by itself
+            int c = va_arg(args, int);
+            printf("%c\n", c);
+        }
+        ++fmt;
+    }
+ 
+    va_end(args);
+}
+ 
 //void bar_vla(unsigned n, int a[*]);
 
 int main(int argc, char* argv[])
