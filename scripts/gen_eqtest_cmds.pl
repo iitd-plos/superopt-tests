65b90f556063ebb9e779150c73c10db209664a48
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-03-12 17:21:47 +0530

    added support for passing extra flags to eq through EXTRA_FLAGS in Makefile

--- scripts/gen_eqtest_cmds.pl
+++ scripts/gen_eqtest_cmds.pl
@@ -16,10 +16,15 @@ my $srcdst_default_compiler_suffix = "gcc.eqchecker.O0.$dst_arch.s";
 
 my $PWD = getcwd;
 
+my $extraflagsarg = $ARGV[4];
+my @extraflags = split('@', $extraflagsarg);
+shift(@extraflags);
+my $extraflagsstr = join('',@extraflags);
+
 my %unroll;
 
 my $cur;
-foreach(my $i = 4; $i <= $#ARGV; $i++) {
+foreach(my $i = 5; $i <= $#ARGV; $i++) {
   my $arg = $ARGV[$i];
   #print "arg = $arg\n";
   if ($arg eq "unroll1") {
@@ -42,8 +47,8 @@ foreach(my $i = 4; $i <= $#ARGV; $i++) {
 foreach my $prog (keys %unroll) {
   my $u = $unroll{$prog};
   if ($compiler_suffix eq "srcdst") {
-    print "python $SUPEROPT_PROJECT_DIR/superopt/utils/eqbin.py -isa $dst_arch $VPATH/$prog\_src.c $PWD/$prog\_dst.$srcdst_default_compiler_suffix.UNROLL$u\n";
+    print "python $SUPEROPT_PROJECT_DIR/superopt/utils/eqbin.py -isa $dst_arch -extra_flags=$extraflagsstr $VPATH/$prog\_src.c $PWD/$prog\_dst.$srcdst_default_compiler_suffix.UNROLL$u\n";
   } else {
-    print "python $SUPEROPT_PROJECT_DIR/superopt/utils/eqbin.py -isa $dst_arch $VPATH/$prog.c $PWD/$prog.$compiler_suffix.UNROLL$u\n";
+    print "python $SUPEROPT_PROJECT_DIR/superopt/utils/eqbin.py -isa $dst_arch -extra_flags=$extraflagsstr $VPATH/$prog.c $PWD/$prog.$compiler_suffix.UNROLL$u\n";
   }
 }
