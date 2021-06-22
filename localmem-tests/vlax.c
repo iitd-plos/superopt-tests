fb28c6fa1f94204ebc3a6c15c07af86d9ede30ab
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-06-23 01:17:11 +0530

    localmem-tests: Makefile: enable alloca_global, alloca_linked_list; also remove vlax_0 (moved to alloca_global)

--- localmem-tests/vlax.c
+++ localmem-tests/vlax.c
@@ -1,28 +1,28 @@
 #include <alloca.h>
 
-int vlax_0(char* s, int fd)
-{
-  if (!s)
-    return 0;
-  int n = strlen(s);
-  char* a;
-  if (n < 4096) {
-    a = alloca(n);
-  } else {
-    a = MYmymalloc(n);
-    if (!a) return 0;
-  }
-#pragma clang loop vectorize(disable) unroll(disable)
-  for (int i = 0; i < n; ++i) {
-    MYmyDBG();
-    a[i] = s[i] ^ 1;
-  }
-  int ret = write(fd, a, n);
-  if (!(n < 4096))
-    MYmyfree(a);
-  return ret;
-}
-
+//int vlax_0(char* s, int fd)
+//{
+//  if (!s)
+//    return 0;
+//  int n = strlen(s);
+//  char* a;
+//  if (n < 4096) {
+//    a = alloca(n);
+//  } else {
+//    a = MYmymalloc(n);
+//    if (!a) return 0;
+//  }
+//#pragma clang loop vectorize(disable) unroll(disable)
+//  for (int i = 0; i < n; ++i) {
+//    MYmyDBG();
+//    a[i] = s[i] ^ 1;
+//  }
+//  int ret = write(fd, a, n);
+//  if (!(n < 4096))
+//    MYmyfree(a);
+//  return ret;
+//}
+//
 #define MAX(x, y) ((x > y) ? (x) : (y))
 
 int lcs(const char *s, const char *t, int m, int n)
