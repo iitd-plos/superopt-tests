a4a3e03dcdf666c8e8c9ca0c44aa3a4fc7ae7275
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-06-29 01:46:14 +0530

    localmem-tests: added marker calls to lcs

--- localmem-tests/conditional.c
+++ localmem-tests/conditional.c
@@ -1,7 +1,7 @@
 int conditional_use(int n, int k)
 {
    int a[n];
-   if (k > 0 && k < n) {
+   if (k > 0 && k <= n) {
       a[0] = 0;
       a[k-1] = 10;
       return a[0];
--- localmem-tests/vlax.c
+++ localmem-tests/vlax.c
@@ -1,36 +1,16 @@
-#include <alloca.h>
-
-//int vlax_0(char* s, int fd)
-//{
-//  if (!s)
-//    return 0;
-//  int n = strlen(s);
-//  char* a;
-//  if (n < 4096) {
-//    a = alloca(n);
-//  } else {
-//    a = MYmymalloc(n);
-//    if (!a) return 0;
-//  }
-//#pragma clang loop vectorize(disable) unroll(disable)
-//  for (int i = 0; i < n; ++i) {
-//    MYmyDBG();
-//    a[i] = s[i] ^ 1;
-//  }
-//  int ret = write(fd, a, n);
-//  if (!(n < 4096))
-//    MYmyfree(a);
-//  return ret;
-//}
-//
 #define MAX(x, y) ((x > y) ? (x) : (y))
 
 int lcs(const char *s, const char *t, int m, int n)
 {
   int dp[m+1][n+1];
 
-  for (int i = 0; i <= m; ++i) dp[i][0] = 0;
-  for (int j = 0; j <= n; ++j) dp[0][j] = 0;
+  for (int i = 0; i <= m; ++i) {
+    MYmyDBG();
+    dp[i][0] = 0;
+  }
+  for (int j = 0; j <= n; ++j) {
+    dp[0][j] = 0;
+  }
 
   for (int i = 1; i <= m; i++) {
     MYmyDBG();
