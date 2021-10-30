4ce29daf5cab92ca457638984daef66fef197c94
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-10-30 17:43:13 +0530

    localmem-tests: vla_in_loop_conditional_{continue,exit}: changes so that multiple deallocs are generated

--- localmem-tests/vla_in_loop_conditional_continue.c
+++ localmem-tests/vla_in_loop_conditional_continue.c
@@ -1,12 +1,14 @@
 int vla_in_loop_conditional_continue(int n)
 {
   int ret = 0;
-  for (int i = 1; i < n; ++i) {
+  int i = 1;
+  while (i < n) {
     char t[i];
     int r = MYmyInit(t, i);
     if (r < 0)
       continue;
     ret += t[i-1];
+    ++i;
   }
   return ret;
 }
--- localmem-tests/vla_in_loop_conditional_exit.c
+++ localmem-tests/vla_in_loop_conditional_exit.c
@@ -1,12 +1,14 @@
 int vla_in_loop_conditional_exit(int n)
 {
   int ret = 0;
-  for (int i = 1; i < n; ++i) {
+  int i = 1;
+  while (i < n) {
     char t[i];
     int r = MYmyInit(t, i);
     if (r < 0)
       break;
     ret += t[i-1];
+    ++i;
   }
   return ret;
 }
