ceeb6f0390411bc7385b0f93a8fc71d57382c955
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-12-09 17:27:39 +0530

    localmem-tests: added rodata.c

---
+++ localmem-tests/rodata.c
@@ -0,0 +1,15 @@
+const int cts[] = { 0x66, 0x65, 0x67, 0x60 };
+
+int rodata(int n)
+{
+  int ret = 0;
+  MYmyprintf("Scanning %d chars", n);
+  char t[n];
+  const char zz[] = "0123456789";
+  MYmyscanf("%s",t);
+  for (int i = 0; i < n; ++i)
+    ret += t[i] ^ zz[i%(sizeof zz)];
+  ret = ret + cts[n%((sizeof cts)/sizeof(cts[0]))];
+  MYmyprintf("Returning %d", ret);
+  return ret;
+}
