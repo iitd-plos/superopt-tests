fbd3019c08e8bf2af2fe6c924a10731b1c1a9b64
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2021-12-28 15:18:58 +0530

    localmem-tests: updated rodata.c

--- localmem-tests/rodata.c
+++ localmem-tests/rodata.c
@@ -2,19 +2,17 @@ const int cts[] = { 0x66, 0x65, 0x67, 0x60 };
 
 int rodata(int n)
 {
-  const char* ini = "ZXZZ";
-  int ret = 0;
-  ret += ini[0];
-  ret += ini[1];
-  ret += ini[2];
-  ret += ini[3];
+  char zz[] = "0123456789";
   MYmyprintf("Scanning %d chars", n);
   char t[n];
-  const char zz[] = "0123456789";
   MYmyscanf("%s",t);
-  for (int i = 0; i < n; ++i)
-    ret += t[i] ^ zz[i%(sizeof zz)];
-  ret = ret + cts[n%((sizeof cts)/sizeof(cts[0]))];
+  int ret = 0;
+  for (int i = 0, j = 0; i < n; ++i) {
+    MYmyprintf("Round #...\n", i);
+    zz[j] ^= t[i];
+    if (++j > sizeof zz) j = 0;
+  }
+  ret += zz[0] + cts[n%((sizeof cts)/sizeof(cts[0]))];
   MYmyprintf("Returning %d", ret);
   return ret;
 }
