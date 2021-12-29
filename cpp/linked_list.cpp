352c5fc4f7b80dd0cc0e27088b948cfb5803eb07
Sorav Bansal <sbansal@iitd.ac.in>
2021-12-29 13:46:28 +0530

    updated cpp/linked_list.cpp

--- cpp/linked_list.cpp
+++ cpp/linked_list.cpp
@@ -2,6 +2,7 @@
 // a linked list
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 
 //void* pmem_malloc(size_t);
  
@@ -10,14 +11,15 @@ struct Node {
     struct Node* next;
 };
 
-struct Node* head = NULL;
+struct Node* pmem_head = NULL;
 
 struct Node* head2 = NULL;
 
 void insert(struct Node*& h, int d)
 {
   //struct Node* n = (struct Node*)malloc(sizeof(struct Node));
-  struct Node* n = new Node;
+  struct Node* pmem_n = new Node;
+  struct Node* n = pmem_n;
   //if (!n) {
   //  return;
   //}
@@ -46,11 +48,17 @@ void insert2(struct Node*& h, int d)
 // list with 3 nodes
 int main()
 {
-  insert(head, 1);
-  insert(head, 2);
-  //insert(&head2, 3);
+  insert(pmem_head, 1);
+  insert(pmem_head, 2);
 
+  insert(head2, 3);
   insert2(head2, 4);
+
+  struct Node* next;
+  for (auto iter = head2; iter; iter = next) {
+    next = iter->next;
+    delete iter;
+  }
 
   return 0;
 }
