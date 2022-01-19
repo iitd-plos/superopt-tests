fde26bc5ed573d73b891b9f101549ab1cf6200d5
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2022-01-20 00:00:50 +0530

    localmem-tests: added vla_3_single_loop

---
+++ localmem-tests/vla_3_single_loop.c
@@ -0,0 +1,16 @@
+int vla_2_single_loop(int *a, unsigned n)
+{
+  if (n == 0)
+    return 0;
+
+  int v[n];
+  int w[n];
+  int x[n];
+  for (unsigned i = 0; i < n; ++i) {
+    MYmyDBG();
+    v[i] = a[i]*a[i];
+    w[i] = a[i]+a[i];
+    x[i] = a[i]^a[i];
+  }
+  return MYmybar(v,w,x);
+}
