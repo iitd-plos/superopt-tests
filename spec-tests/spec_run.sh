72d7a2ee6ace56a2d8ee53ec53074d5d4a1aa5e7
Indrajit Banerjee <csy207569@iitd.ac.in>
2022-02-22 16:41:31 +0530

    fix to spec_run script

--- spec-tests/spec_run.sh
+++ spec-tests/spec_run.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 spec_tmpdir="tmpdir"
+spec_run_out="spec_run.out"
 
 run() {
-    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee -a ${spec_tests_out}
+    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee ${spec_run_out}
 }
 
 run_enabled_spec_eqclass() {
-    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --enable-manual-spec-eqclass=true --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee -a ${spec_tests_out}
+    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --enable-manual-spec-eqclass=true --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee ${spec_run_out}
 }
 
 debug() {
-    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --dyn-debug=correlate=3,stability=3,spec_as_src=3,spec_dbg=3,reconcile_ce=3 --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee -a ${spec_tests_out}
+    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --dyn-debug=correlate=3,stability=3,spec_as_src=3,spec_dbg=3,reconcile_ce=3 --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee ${spec_run_out}
 }
 
 debug_enabled_spec_eqclass() {
-    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --dyn-debug=correlate=3,stability=3,spec_as_src=3,spec_dbg=3,reconcile_ce=3 --enable-manual-spec-eqclass=true --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee -a ${spec_tests_out}
+    ${SUPEROPT_INSTALL_DIR}/bin/eq32 --tmpdir-path=${spec_tmpdir} --dyn-debug=correlate=3,stability=3,spec_as_src=3,spec_dbg=3,reconcile_ce=3 --enable-manual-spec-eqclass=true --spec-iospecs="$1".iospecs "$1".spec "$1".c | tee ${spec_run_out}
 }
