9b660beb922747eebc072f1a6a0a906de016720a
Aditya Senthilnathan <aditsep99@gmail.com>
2022-04-06 13:04:56 +0530

    Added answer.txt to most testcases in llvm2llvm

---
+++ llvm2llvm/test7.2/src.s
@@ -0,0 +1,16 @@
+	.text
+	.file	"src.ll"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	movl	8(%esp), %eax
+	addl	4(%esp), %eax
+	retl
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.section	".note.GNU-stack","",@progbits
