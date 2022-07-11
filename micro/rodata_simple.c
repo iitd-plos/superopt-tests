04fc52f92a1bd7e71bcd02795954874c43fe49dd
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2022-07-11 23:12:52 +0530

    micro: added fcall_taking_locals and rodata_simple

---
+++ micro/rodata_simple.c
@@ -0,0 +1,11 @@
+const char hw[] = "hello world";
+
+int rodata_simple(const char *s)
+{
+  const char* p = s != 0 ? s : hw;
+  int ret = 0;
+  for (int i = 0; i < MYmystrlen(p); ++i) {
+    ret += *p;
+  }
+  return ret;
+}
