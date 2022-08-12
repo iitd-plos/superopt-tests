a3dd26e40da23fcef2d60d849dc5a444579d444f
Indrajit Banerjee <csy207569@iitd.ac.in>
2022-08-12 12:54:39 +0530

    add spec2tfg convenience script

--- spec-tests/spec2tfg.sh
+++ spec-tests/spec2tfg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-spec2tfg() {
-	${SUPEROPT_PROJECT_DIR}/superopt/build/etfg_x64/spec2tfg "$1".spec -o "$1".etfg -ast-dump "$1".ast
+run_spec2tfg() {
+	${SUPEROPT_PROJECT_DIR}/superopt/build/etfg_x64/spec2tfg "$1".spec -o "$1".etfg -ast-dump "$1".ast -dyn-debug sort_canonical
 }
 
