0199fdd7171ec0782f8d4423e92839c3e4fdda81
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2022-09-15 18:55:00 +0530

    localmem-tests: alloca_conditional: disable vectorization/unrolling using pragma

--- localmem-tests/alloca_linked_list.c
+++ localmem-tests/alloca_linked_list.c
@@ -12,6 +12,8 @@ int alloca_linked_list()
 
   Node* hd = 0;
   for (int i = 0; i < n; ++i) {
+    //DBG(__LINE__); // fcall with arg in loop with alloca() will not get correlated because a loc will not be created for arg slot, thus missing the arg slot and llvm arg correlation
+                     // loc creation will fail because of avail-exprs analysis' incompleteness due to shallow-deep problem
     Node* tmp = alloca(sizeof(Node));
     tmp->data = MYmynext_data();
     tmp->next = hd; hd = tmp;
