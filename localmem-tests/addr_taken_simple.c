1490e77bb1902165fb866d7a9778ccfc66ab7882
Sorav Bansal <sbansal@iitd.ac.in>
2022-10-31 13:51:49 +0530

    added a "long long" local in addr_taken_simple.c

--- localmem-tests/addr_taken_simple.c
+++ localmem-tests/addr_taken_simple.c
@@ -1,8 +1,11 @@
 void MYmyfoo(int*);
+void MYmybar(long long*);
 
 int addr_taken_simple(void)
 {
   int ret;
+  long long ret2;
   MYmyfoo(&ret);
-  return ret;
+  MYmybar(&ret2);
+  return ret + ret2;
 }
