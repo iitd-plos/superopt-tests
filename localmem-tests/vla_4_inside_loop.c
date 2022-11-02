416111aad76a7a705996d9a733fc77f499398dca
Sorav Bansal <sbansal@iitd.ac.in>
2022-11-02 17:09:54 +0530

    Add localmem-tests/vla_{3,4}_{inside,single}_loop

---
+++ localmem-tests/vla_4_inside_loop.c
@@ -0,0 +1,13 @@
+int vla_4_inside_loop(int *a, unsigned n)
+{
+  int ret = 0;
+  for (unsigned i = 1; i < n; ++i) {
+   MYmyDBG();
+   int v[4*i];
+   int w[4*i];
+   int x[4*i];
+   int y[4*i];
+   ret += MYmyfoo(v,w,x,y,i);
+  }
+  return ret;
+}
