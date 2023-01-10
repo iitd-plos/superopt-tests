c800099c61ff65235a128c85419ea5ad14c4ce5d
Indrajit Banerjee <csy207569@iitd.ac.in>
2023-01-10 19:55:50 +0530

    remove axpred-solver-depth and update benchmark dump
    Removed axpred-solver-depth since it uses automatic max depth inference.

--- spec-tests/par_script.sh
+++ spec-tests/par_script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 spec_par_tmpdir="par_tmpdir"
-axpred_solver_max_depth=4
-cmd_name="${SUPEROPT_INSTALL_DIR}/bin/eq32 --axpred-solver-max-depth=${axpred_solver_max_depth} --disable-dst-to-src-submap --smt-query-timeout=5"
+cmd_name="${SUPEROPT_INSTALL_DIR}/bin/eq32 --disable-dst-to-src-submap --smt-query-timeout=5"
 
 par_run() {
   ${cmd_name} --tmpdir-path=${spec_par_tmpdir} --proof="$1".proof --spec-iospecs="$1".iospecs "$1".spec "$1".c >& "$1".out
--- spec-tests/run.sh
+++ spec-tests/run.sh
@@ -2,15 +2,14 @@
 
 spec_run_tmpdir="run_tmpdir"
 spec_run_out="run.out"
-axpred_solver_max_depth=4
-cmd_name="${SUPEROPT_INSTALL_DIR}/bin/eq32 --axpred-solver-max-depth=${axpred_solver_max_depth} --disable-dst-to-src-submap --smt-query-timeout=5"
+cmd_name="${SUPEROPT_INSTALL_DIR}/bin/eq32 --disable-dst-to-src-submap --smt-query-timeout=5"
 
 speceq_run() {
   ${cmd_name} --tmpdir-path=${spec_run_tmpdir} --proof="$1".proof --dyn-debug=invariants_dump --spec-iospecs="$1".iospecs "$1".spec "$1".c |& tee ${spec_run_out}
 }
 
 speceq_debug() {
-  ${cmd_name} --tmpdir-path=${spec_run_tmpdir} --proof="$1".proof --dyn-debug=invariants_dump --spec-iospecs="$1".iospecs "$1".spec "$1".c |& tee ${spec_run_out}
+  ${cmd_name} --tmpdir-path=${spec_run_tmpdir} --proof="$1".proof --dyn-debug=invariants_dump,axpred_depth=2 --spec-iospecs="$1".iospecs "$1".spec "$1".c |& tee ${spec_run_out}
 }
 
 speceq_debug_replay() {
