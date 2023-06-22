0a0a39a08b2e95a5c3bd47b3b73198c58fc3b13e
jaiarora0011 <jaiarora0011@gmail.com>
2023-06-22 18:01:14 +0530

    Added to option to use the alternate ineq-checking approach in benchmarking

--- inequivalence/benchmarking.py
+++ inequivalence/benchmarking.py
@@ -14,26 +14,12 @@ import xml.etree.ElementTree as ET
 
 # Contains benchmarking information
 # FORMAT: each line represents one function
-## {fname},{lib_1},{lib_2} . . .
-# input_file = sys.argv[1]
-
-FLAGS="correctness_covers,correlate,invariants_map=2"
-FAILSET_PRUNE=-1
-FAILSET_SORT="next-cross"
-FAILSET_MU=16
-UNROLL_FACTOR_MIN=1
-UNROLL_FACTOR_MAX=64
-UNROLL_FACTOR_RATIO=2
-CE_BOUND=22
-TIMEOUT=45*60
-NJOBS=30
-VMEM_LIMIT=32*1024*1024 # 32GB
-NUM_CHUNKS=1
-
-USE_ASSUMES = False
+## {fname},{lib_1}.c,{lib_2}.S . . .
 
 class RunConfig():
     def __init__(self,
+                 input_file: str,
+                 output_dir: str,
                  failset_prune: int,
                  failset_sort: str,
                  loop_bound: int,
@@ -41,13 +27,16 @@ class RunConfig():
                  unroll_factor_max: int,
                  unroll_factor_ratio: int,
                  ce_bound: int,
-                 timeout: float,
+                 timeout: int,
                  njobs: int,
                  vmem_limit: int,
                  num_chunks: int,
                  use_assumes: bool,
                  ineq_individual_tfgs: bool,
+                 disable_inequivalence: bool,
                  debug_flags: str):
+        self.input_file = input_file
+        self.output_dir = output_dir
         self.failset_prune = failset_prune
         self.failset_sort = failset_sort
         self.loop_bound = loop_bound
@@ -61,6 +50,7 @@ class RunConfig():
         self.num_chunks = num_chunks
         self.use_assumes = use_assumes
         self.ineq_individual_tfgs = ineq_individual_tfgs
+        self.disable_inequivalence = disable_inequivalence
         self.debug_flags = debug_flags
 
 # .S files could use preprocessor directives. If they do, then eq32 currently classifies those as C source files
@@ -87,57 +77,70 @@ def get_bench_path(fname: str, lib: str) -> str:
 def is_c_file(file_name: str) -> bool:
     return file_name.split('.')[-1] == 'c'
 
+def is_asm_file(file_name: str) -> bool:
+    extension = file_name.split('.')[-1]
+    return extension == 's' or extension == 'S'
+
 def get_eq_command_from_config(eqrun_cfg: RunConfig, fname: str, src: str, dst: str, unroll: int) -> str:
     tokens = ['eq32']
     if eqrun_cfg.debug_flags != '':
         tokens.append(f'--dyn-debug={eqrun_cfg.debug_flags}')
     tokens.append(f'--unroll-factor={unroll}')
-    if eqrun_cfg.ineq_individual_tfgs:
-        tokens.append('--ineq-individual-tfgs')
+
+    # Inequivalence Checking Flags
+    if eqrun_cfg.disable_inequivalence:
+        tokens.append('--disable-inequivalence')
     else:
-        tokens.append(f'--failset-prune={eqrun_cfg.failset_prune}')
-        tokens.append(f'--failset-sort={eqrun_cfg.failset_sort}')
-        tokens.append(f'--ce-bound={eqrun_cfg.ce_bound}')
+        tokens.append(f'--failset-mu={eqrun_cfg.loop_bound}')
+        tokens.append(f'--ce-output={eqrun_cfg.output_dir}/counterexamples/ineq.{fname}-{src}-{dst}-{unroll}-{eqrun_cfg.loop_bound}.ce')
+        if eqrun_cfg.ineq_individual_tfgs:
+            tokens.append('--ineq-individual-tfgs')
+        else:
+            tokens.append(f'--failset-prune={eqrun_cfg.failset_prune}')
+            tokens.append(f'--failset-sort={eqrun_cfg.failset_sort}')
+            tokens.append(f'--ce-bound={eqrun_cfg.ce_bound}')
+
     tokens.append(f'{get_bench_path(fname, src)}')
     tokens.append(f'--dst={get_bench_path(fname, dst)}')
-    # TODO: --ce-output, --tmpdir-path, --disable-inequivalence
+
+    tokens.append(f'--tmpdir-path={eqrun_cfg.output_dir}/tmpdir/ineq.{fname}-{src}-{dst}-{unroll}-{eqrun_cfg.loop_bound}')
+    if eqrun_cfg.use_assumes:
+        # Provide an assumes file for the source (C) program
+        # TODO: Handle the case when the source is an ASM program
+        if is_c_file(src):
+            src_prefix = src.split('.')[0]
+            tokens.append(f'--assume=assumes/{fname}-{src_prefix}.assumes')
     return ' '.join(tokens)
 
 def get_eq_command_with_vmem_limit(eqrun_cfg: RunConfig, fname: str, src: str, dst: str, unroll: int) -> str:
     command = get_eq_command_from_config(eqrun_cfg, fname, src, dst, unroll)
     return f'ulimit -v {eqrun_cfg.vmem_limit}; {command}'
  
-def run_ineq_checker(fname: str, src_lib: str, dst_lib: str, log_file:TextIOWrapper, classes: equivalence_classes, unroll):
+def run_ineq_checker(fname: str, src_lib: str, dst_lib: str, log_file: TextIOWrapper, classes: equivalence_classes, unroll: int, eqrun_cfg: RunConfig) -> bool:
     u = classes.get_leader(src_lib)
     v = classes.get_leader(dst_lib)
+
     if u == v:
         # They already lie in the same equivalence class, no need to run the tool
-        return False, False
+        return False
     else:
         if v in classes.inequivalent_sets[u] or u in classes.inequivalent_sets[v]:
             # They are already inequivalent, no need to run the tool
-            return False, False
+            return False
+
     RETRY = False
-    TIMEOUT_OCCURED = False
-    if not USE_ASSUMES:
-        out_file = open(f'logs/{fname}-{src_lib}-{dst_lib}.proof', 'w')
-    else:
-        out_file = open(f'logs/{fname}-{src_lib}-{dst_lib}-assumes.proof', 'w')
-    if not USE_ASSUMES:
-        command = f'ulimit -v {VMEM_LIMIT}; eq32 --dyn-debug={FLAGS} --failset-prune={FAILSET_PRUNE} --failset-sort={FAILSET_SORT} --failset-mu={FAILSET_MU} --unroll-factor={unroll} --ce_bound={CE_BOUND} --xml-output=counterexamples/{fname}-{src_lib}-{dst_lib}.xml {get_bench_path(fname, src_lib)} {get_bench_path(fname, dst_lib)}'
-    else:
-        src_lib_ = src_lib.split('.')[0]
-        if not is_c_file(src_lib):
-            assert False, f'{src_lib} is not a c file, cannot use assumes'
-        command = f'ulimit -v {VMEM_LIMIT}; eq32 --dyn-debug={FLAGS} --failset-prune={FAILSET_PRUNE} --failset-sort={FAILSET_SORT} --failset-mu={FAILSET_MU} --unroll-factor={unroll} --ce_bound={CE_BOUND} --xml-output=counterexamples/{fname}-{src_lib}-{dst_lib}-assumes.xml --assume=assumes/{fname}-{src_lib_}.assumes {get_bench_path(fname, src_lib)} {get_bench_path(fname, dst_lib)}'
+    out_file = open(f'{eqrun_cfg.output_dir}/logs/{fname}-{src_lib}-{dst_lib}.proof', 'w')
+
+    command = get_eq_command_with_vmem_limit(eqrun_cfg, fname, src_lib, dst_lib, unroll)
     # tokens = shlex.split(command)
     p = None
     try:
-        log_file.write(f'\tRunning the tool for benchmark {fname} with src={src_lib}, dst={dst_lib}, with unroll_factor={unroll}\n')
+        log_file.write(f'\tRunning the tool for benchmark {fname} with src={src_lib}, dst={dst_lib}, at unroll_factor={unroll}\n')
         start_time = time.perf_counter()
         p = subprocess.Popen(command, stdout=out_file, stderr=out_file, shell=True, preexec_fn=os.setsid)
-        p.wait(TIMEOUT)
+        p.wait(eqrun_cfg.timeout)
         end_time = time.perf_counter()
+        # TODO: Cannot use return code to check tool result
         if p.returncode == 0:
             log_file.write(f'\t\tProved {fname} to be equivalent for {src_lib}, {dst_lib}.\n')
             classes.merge(src_lib, dst_lib)
@@ -153,39 +156,34 @@ def run_ineq_checker(fname: str, src_lib: str, dst_lib: str, log_file:TextIOWrap
         os.killpg(os.getpgid(p.pid), signal.SIGTERM)
         log_file.write(f'\t\tTimeout for benchmark {fname} with src={src_lib}, dst={dst_lib}\n')
         RETRY = True
-        TIMEOUT_OCCURED = True
         log_file.write(f'\tTimeout occurred\n')
         classes.add_run_info(fname, src_lib, dst_lib, unroll, p.returncode, None, True)
     finally:
         out_file.close()
         log_file.flush()
-    return RETRY, TIMEOUT_OCCURED
+    return RETRY
 
-def run_benchmark(strings: str, tag: str, classes: equivalence_classes):
+def run_benchmark(strings: str, tag: str, classes: equivalence_classes, eqrun_cfg: RunConfig):
     # strings = [s.strip() for s in line.split(',')]
     fname = strings[0]
     libraries = strings[1:]
-    # classes = equivalence_classes(fname, libraries)
-    log_file_name = ''
-    if not USE_ASSUMES:
-        log_file_name = f'stats/{fname}-{tag}.log'
-    else:
-        log_file_name = f'stats/{fname}-{tag}-assumes.log'
+
+    log_file_name = f'{eqrun_cfg.output_dir}/stats/{fname}-{tag}.log'
     log_file = open(log_file_name, 'w')
     log_file.write(f'Running benchmarks for {fname}\n')
     for pair in itertools.combinations(libraries, 2):
         l1, l2 = pair
         if (l1, l2) in classes.cache:
             continue
-        unroll = UNROLL_FACTOR_MIN
-        while unroll <= UNROLL_FACTOR_MAX:
-            retry, _ = run_ineq_checker(fname, l1, l2, log_file, classes, unroll)
+        unroll = eqrun_cfg.unroll_factor_min
+        while unroll <= eqrun_cfg.unroll_factor_max:
+            retry = run_ineq_checker(fname, l1, l2, log_file, classes, unroll, eqrun_cfg)
             if not retry:
                 break
-            retry, _ = run_ineq_checker(fname, l2, l1, log_file, classes, unroll)
+            retry = run_ineq_checker(fname, l2, l1, log_file, classes, unroll, eqrun_cfg)
             if not retry:
                 break
-            unroll *= UNROLL_FACTOR_RATIO
+            unroll *= eqrun_cfg.unroll_factor_ratio
         classes.add_cache(l1, l2)
     log_file.write(f'Ran all experiments for {fname}\n')
     classes.print(log_file)
@@ -197,16 +195,12 @@ def split(a, n):
     k, m = divmod(len(a), n)
     return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
-def divide_conquer(line: str, tag: str, num_chunks):
+def divide_conquer(line: str, tag: str, eqrun_cfg: RunConfig):
     strings = [s.strip() for s in line.split(',')]
     fname = strings[0]
     libraries = strings[1:]
 
-    eq_class_file = ''
-    if not USE_ASSUMES:
-        eq_class_file = f'eq_classes/{fname}-{tag}-classes.pkl'
-    else:
-        eq_class_file = f'eq_classes/{fname}-{tag}-assumes-classes.pkl'
+    eq_class_file = f'eq_classes/{fname}-{tag}-classes.pkl'
 
     split_classes = len(libraries) > 2 and num_chunks != 1
     classes = None
@@ -224,22 +218,18 @@ def divide_conquer(line: str, tag: str, num_chunks):
         num_chunks = min(num_chunks, len(libraries))
         chunks = split(libraries, num_chunks)
         # for each chunk in chunks, run the eq_checker, get the classes, merge them, and rerun the eq_checker
-        all_classes = Parallel(n_jobs=num_chunks)(delayed(run_benchmark)([fname]+chunk, tag+'-'+str(i), equivalence_classes(fname, chunk)) for i, chunk in enumerate(chunks))
+        all_classes = Parallel(n_jobs=num_chunks)(delayed(run_benchmark)([fname]+chunk, tag+'-'+str(i), equivalence_classes(fname, chunk), eqrun_cfg) for i, chunk in enumerate(chunks))
 
         classes = merge_all_classes(all_classes)
     else:
         classes = equivalence_classes(fname, libraries) if classes is None else classes
     
-    classes = run_benchmark(strings, tag, classes)
+    classes = run_benchmark(strings, tag, classes, eqrun_cfg)
 
     with open(eq_class_file, 'wb') as f:
         pickle.dump(classes, f)
 
-    stat_file_name = ''
-    if not USE_ASSUMES:
-        stat_file_name = f'stats/{fname}-{tag}-stats.csv'
-    else:
-        stat_file_name = f'stats/{fname}-{tag}-assumes-stats.csv'
+    stat_file_name = f'stats/{fname}-{tag}-stats.csv'
     
     with open(stat_file_name, 'w') as f:
         for info in classes.get_runs():
@@ -248,33 +238,31 @@ def divide_conquer(line: str, tag: str, num_chunks):
     return [run for run in classes.get_runs() if run.is_ineq()]
 
 # Runs the experiments while keeping track of equivalence classes
-def run_experiments(input_file):
-    file = open(input_file, 'r')
+def run_experiments(eqrun_cfg: RunConfig):
+    file = open(eqrun_cfg.input_file, 'r')
     lines = file.readlines()
     lines = [line.strip() for line in lines]
     file.close()
 
-    inequivalences = Parallel(n_jobs=min(NJOBS, len(lines)))(delayed(divide_conquer)(line, str(i), NUM_CHUNKS) for i, line in enumerate(lines))
+    inequivalences = Parallel(n_jobs=min(eqrun_cfg.njobs, len(lines)))(delayed(divide_conquer)(line, str(i), eqrun_cfg) for i, line in enumerate(lines))
 
     ret = []
     for ineq in inequivalences:
         ret = ret + ineq
     
-    ineq_file = f'stats/ineq-{input_file}'
+    ineq_file = f'{eqrun_cfg.output_dir}/stats/ineq-{eqrun_cfg.input_file}'
     with open(ineq_file, 'w') as f:
         for info in ret:
             f.write(f'{info.to_string()}\n')
 
 # Takes the computed classes and generates the classes in dot format
-def analysis():
-    pickled_files = glob.glob('eq_classes/*-classes.pkl')
+def analysis(eqrun_cfg: RunConfig):
+    pickled_files = glob.glob(f'{eqrun_cfg.output_dir}/eq_classes/*-classes.pkl')
     pickled_files.sort()
     groups = dict()
     for file in pickled_files:
         toks = file.split('-')
-        fname = toks[0].split('/')[1]
-        if toks[-2] == 'assumes':
-            fname += '-assumes'
+        fname = toks[0].split('/')[2]
         if fname not in groups:
             groups[fname] = []
         groups[fname].append(file)
@@ -305,8 +293,8 @@ def plot_hist(times, title, xlabel='Time (in seconds)'):
     plt.clf()
 
 # Analyses the logs to generate statistics
-def graph_gen():
-    stats = glob.glob('stats/*-stats.csv')
+def graph_gen(eqrun_cfg: RunConfig):
+    stats = glob.glob(f'{eqrun_cfg.output_dir}/stats/*-stats.csv')
     stats.sort()
     NTIMEOUTS = 0
     completion_times = []
@@ -385,31 +373,60 @@ def update_eq_classes():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Inequivalence Checking Benchmarking')
-    parser.add_argument('--mode', action='store', type=str, help='Denotes the function of the script. Options = [run|analysis|graph_gen|update_eq_classes]', default='')
-    parser.add_argument('--input_file', action='store', type=str, help='File containing the benchmarks to run', default='')
-    parser.add_argument('--failset_mu', action='store', type=int, help='Denotes the mu to be used in inequivalence checking', default=FAILSET_MU)
-    parser.add_argument('--unroll_factor', action='store', type=int, default=UNROLL_FACTOR_MAX)
-    parser.add_argument('--ce_bound', action='store', type=int, help='Sets a bound on the counter examples to be generated', default=CE_BOUND)
-    parser.add_argument('--timeout', action='store', type=int, help='Timeout for each run (in seconds)', default=TIMEOUT)
-    parser.add_argument('--njobs', action='store', type=int, default=NJOBS)
-    parser.add_argument('--assumes', action='store_true')
-    parser.add_argument('--num_chunks', action='store', type=int, help='Number of chunks into which each experiment is divided into', default=NUM_CHUNKS)
+
+    # Flags pertaining to benchmarking
+    parser.add_argument('--mode', action='store', type=str, help='Denotes the function of the script. Options = [run|analysis|graph-gen|update-eq-classes]', default='')
+    parser.add_argument('--input-file', action='store', type=str, help='File containing the benchmarks to run', default='')
+    parser.add_argument('--timeout', action='store', type=int, help='Timeout for each run (in seconds)', default=60*60)
+    parser.add_argument('--njobs', action='store', type=int, default=25)
+    parser.add_argument('--num-chunks', action='store', type=int, help='Number of chunks into which each experiment is divided into', default=1)
+    parser.add_argument('--vmem-limit', action='store', type=int, help='Virtual memory limit in (KB) to be used for each process', default=32*1024*1024) # 32 GB
+    parser.add_argument('--output-dir', action='store', type=str, help='Directory where all the files will be written', default='')
+
+    # Flags pertaining to eq32
+    parser.add_argument('--dyn-debug', action='store', type=str, help='Debug flags for the tool', default='')
+    parser.add_argument('--failset-prune', action='store', type=int, help='Denotes the number of failed CGs to be used for inequivalence checking. -1 denotes no pruning.', default=-1)
+    parser.add_argument('--failset-sort', action='store', type=str, help='Denotes the strategy used to rank failed CGs', default='next-cross')
+    parser.add_argument('--failset-mu', action='store', type=int, help='Denotes the loop bound to be used in inequivalence checking', default=4)
+    parser.add_argument('--unroll-factor-min', action='store', type=int, default=1)
+    parser.add_argument('--unroll-factor-max', action='store', type=int, default=4)
+    parser.add_argument('--unroll-factor-ratio', action='store', type=int, default=2)
+    parser.add_argument('--ce-bound', action='store', type=int, help='Sets a bound on the counter examples to be generated', default=20)
+    parser.add_argument('--assumes', action='store_true', help='Run the tool with appropriate assumes for each function')
+    parser.add_argument('--ineq-individual-tfgs', action='store_true', help='Enable the alternate inequivalence checking approach')
+    parser.add_argument('--disable-inequivalence', action='store_true', help='Disable Inequivalence Checking')
+
     args = parser.parse_args()
-    assert(args.mode != '')
-    assert args.mode == 'run' or args.mode == 'analysis' or args.mode == 'graph_gen' or args.mode == 'update_eq_classes', f'Invalid Mode'
-    FAILSET_MU = args.failset_mu
-    UNROLL_FACTOR_MAX = args.unroll_factor
-    CE_BOUND = args.ce_bound
-    TIMEOUT = args.timeout
-    NJOBS = args.njobs
-    USE_ASSUMES = args.assumes
-    NUM_CHUNKS = args.num_chunks
+    argsMap = args.__dict__
+    assert(argsMap['mode'] != '')
+    assert argsMap['mode'] == 'run' or argsMap['mode'] == 'analysis' or argsMap['mode'] == 'graph-gen' or argsMap['mode'] == 'update-eq-classes', f'Invalid Mode'
+    assert(argsMap['input-file'] != '')
+
     print(f'My pid is {os.getpid()}, pgid is {os.getpgid(0)}')
-    if args.mode == 'run':
-        run_experiments(args.input_file)
-    elif args.mode == 'analysis':
-        analysis()
-    elif args.mode == 'graph_gen':
+
+    eqrun_cfg = RunConfig(argsMap['input-file'],
+                          argsMap['output-dir'],
+                          argsMap['failset-prune'],
+                          argsMap['failset-sort'],
+                          argsMap['failset-mu'],
+                          argsMap['unroll-factor-min'],
+                          argsMap['unroll-factor-max'],
+                          argsMap['unroll-factor-ratio'],
+                          argsMap['ce-bound'],
+                          argsMap['timeout'],
+                          argsMap['njobs'],
+                          argsMap['vmem-limit'],
+                          argsMap['num-chunks'],
+                          argsMap['assumes'],
+                          argsMap['ineq-individual-tfgs'],
+                          argsMap['disable-inequivalence'],
+                          argsMap['dyn-debug'])
+
+    if argsMap['mode'] == 'run':
+        run_experiments(eqrun_cfg)
+    elif argsMap['mode'] == 'analysis':
+        analysis(eqrun_cfg)
+    elif argsMap['mode'] == 'graph-gen':
         graph_gen()
-    elif args.mode == 'update_eq_classes':
+    elif argsMap['mode'] == 'update-eq-classes':
         update_eq_classes()
